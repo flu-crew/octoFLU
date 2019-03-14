4809e98b48d89c9c85cf6439e2c4526c79de00b5
j23414 <jenchang@iastate.edu>
2019-03-13 20:16:43 -0500

    Expanded to pull multiple annotations from the tree

--- nn_classifier.R
+++ nn_classifier.R
@@ -1,7 +1,8 @@
-#Michael Zeller 3/12/2019
-#Input: Tree file (newick/nexus). Tips should be labeled as name|clade. Query sequences should be 
+#! /usr/bin/env Rscript
+# Auth: Michael Zeller 3/12/2019
+# Input: Tree file (newick/nexus). Tips should be labeled as name|clade. Query sequences should be 
 # name only, without '|
-#Output: Nearest neighbor clade assignment based on patristic tree distance
+# Output: Nearest neighbor clade assignment based on patristic tree distance
 
 #Load needed libraries
 library(ape)
@@ -9,11 +10,17 @@ library(ape)
 #Grab R from commandline arguments, or print usage
 treeFile <- ""
 args <- commandArgs(trailingOnly=TRUE)
+
 if(length(args) < 1) {
   print("Usage: Rscript nn_prototype.R path-to-tree-file")
   quit()
 } else {
   treeFile <- args[1]
+  if(length(args)>1){ # Allow user to specify column
+    col_annotated=as.numeric(args[-1]) # all but first, can pull out multiple annotations
+  }else{
+    col_annotated=2
+  }
 }
 
 #Load user specified tree file
@@ -27,7 +34,7 @@ taxa <- colnames(pdm)
 for (i in 1:length(taxa)) {
   #Check if clade designation is present, skip if so
   defSplit = strsplit(taxa[i],"|", fixed=TRUE)
-  if(length(defSplit[[1]]) >=2) {       #Assumption! One delimiter
+  if(length(defSplit[[1]]) >=min(col_annotated)) {       #Assumption! One delimiter (JC: can we make this more robust?)
     next
   }
   
@@ -35,8 +42,14 @@ for (i in 1:length(taxa)) {
   orderedList = sort(pdm[,i])
   for (j in 1:length(orderedList)) {
     compSplit = strsplit(names(orderedList[j]),"|", fixed=TRUE)
-    if(length(compSplit[[1]]) >=2) {
-      print(paste(taxa[i],"is closest to ", compSplit[[1]][2], sep = " "))
+    
+    if(length(compSplit[[1]]) >=min(col_annotated)) {
+      out_str=taxa[i]
+      for (ca in col_annotated){  # print out each column of annotations
+        out_str=paste(out_str,compSplit[[1]][ca], sep='\t')
+      }
+      cat(out_str,"\n")
+      #print(paste(taxa[i],"is closest to ", compSplit[[1]][col_annotated], sep = " "))
       break
     }
   }

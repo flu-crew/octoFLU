bd8893b623b6fadae689efe20222ea3f66aa004e
Zeller <michael.zeller@ARSIAAM2AV2513D.local>
2019-07-26 16:43:45 -0500

    Rewriting bash script in python, because...

---
+++ pipeline.py
@@ -0,0 +1,168 @@
+import os
+import sys
+import subprocess
+from shutil import which
+
+# ===== Input and Output
+inputFile = sys.argv[1]
+baseName = os.path.basename(inputFile)
+outDir = baseName + "_output"
+
+#print(inputFile)
+#print(outDir)
+
+# ===== Create Output Directory if not already created
+#[ -d ${OUTDIR} ] || mkdir ${OUTDIR} 
+if not os.path.exists(outDir):
+	os.mkdir(outDir)
+else:
+	sys.exit("Directory " + outDir + " already exists")
+
+# ===== Connect your reference here
+reference = "reference_data/reference.fa"
+
+# ===== Connect your programs here, assuming installed on your system
+BLASTN = "/Users/michael.zeller/ncbi_blast/blastn"
+MAKEBLASTDB = "/Users/michael.zeller/ncbi_blast/makeblastdb"
+SMOF = "smof"
+MAFFT = "/usr/local/bin/mafft"
+FASTTREE = "/Users/michael.zeller/FastTree/FastTree"
+NN_CLASS = "treedist.py"
+
+# ===== Uncomment and connect your programs here using full path names
+# BLASTN=/usr/local/bin/blastn
+# MAKEBLASTDB=/usr/local/bin/makeblastdb
+# SMOF=/usr/local/bin/smof
+# MAFFT=/usr/local/bin/mafft
+# FASTTREE=usr/local/bin/FastTree
+# NN_CLASS=nn_classifier.R
+# ANNOT_FASTA=annotate_headers.pl
+
+# ===== Check if dependencies are available, quit if not
+
+# Attempt to use python3, but if not there check if python is python3
+PYTHON = "python3"
+if sys.version_info[0] < 3:
+    system.exit("Must be using Python 3")
+
+"""
+# Attempt to use multiprocessor version, but if not there use single processor
+if [ -z `which FastTreeMP` ]; then
+    FASTTREE=FastTree
+else
+    FASTTREE=FastTreeMP
+fi
+echo $FASTTREE
+"""
+
+# Formal check of dependencies
+Err = 0
+if which(BLASTN) is None:
+	print("blastn      .... need to install")
+	Err = 1
+else:
+	print("blastn      .... good")
+
+if which(MAKEBLASTDB) is None:
+	print("makeblastdb      .... need to install")
+	Err = 1
+else:
+	print("makeblastdb      .... good")
+	
+if which(MAFFT) is None:
+	print("mafft      .... need to install")
+	Err = 1
+else:
+	print("mafft      .... good")
+	
+if which(FASTTREE) is None:
+	print("fastTree      .... need to install")
+	Err = 1
+else:
+	print("fastTree      .... good")
+	
+if which(SMOF) is None:
+	print("smof      .... need to install")
+	Err = 1
+else:
+	print("smof      .... good")
+	
+if (Err == 1):
+    system.exit("Link or install any of your 'need to install' programs above")
+	
+
+# ===== Remove pipes in query header
+with open(inputFile, 'r') as file :
+  fileData = file.read()
+fileData = fileData.replace('|', '_')
+with open(baseName + ".clean", 'w') as file:
+  file.write(fileData)
+  
+# ===== Create your Blast Database
+# ${MAKEBLASTDB} -in ${REFERENCE} -parse_seqids -dbtype nucl      # requires no spaces in header
+subprocess.call([MAKEBLASTDB,"-in",reference,"-dbtype","nucl"])
+
+# ===== Search your Blast Database
+subprocess.call([BLASTN,"-db",reference,"-query",baseName + ".clean","-num_alignments","1","-outfmt","6","-out",outDir + "/blast_output.txt"])
+
+print("... results in " + outDir + "/blast_output.txt")
+
+# ===== Split out query into 8 segments
+H1list = []
+H3list = []
+N1list = []
+N2list = []
+PB2list = []
+PB1list = []
+PAlist = []
+NPlist = []
+Mlist = []
+NSlist = []
+
+with open(outDir + "/blast_output.txt", 'r') as file :
+	for line in file:
+		fields = line.strip().split()
+		if("|H1|" in fields[1]):
+			H1list.append(fields[0])
+		if("|H3|" in fields[1]):
+			H3list.append(fields[0])
+		if("|N1|" in fields[1]):
+			N1list.append(fields[0])
+		if("|N2|" in fields[1]):
+			N2list.append(fields[0])
+		if("|PB2|" in fields[1]):
+			PB2list.append(fields[0])
+		if("|PB1|" in fields[1]):
+			PB1list.append(fields[0])
+		if("|PA|" in fields[1]):
+			PAlist.append(fields[0])
+		if("|NP|" in fields[1]):
+			NPlist.append(fields[0])
+		if("|M|" in fields[1]):
+			Mlist.append(fields[0])
+		if("|NS|" in fields[1]):
+			NSlist.append(fields[0])
+			
+with open(outDir + "/H1.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in H1list])
+with open(outDir + "/H3.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in H3list])
+with open(outDir + "/N1.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in N1list])
+with open(outDir + "/N2.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in N2list])
+with open(outDir + "/PB2.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in PB2list])
+with open(outDir + "/PB1.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in PB1list])
+with open(outDir + "/PA.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in PAlist])
+with open(outDir + "/NP.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in NPlist])
+with open(outDir + "/M.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in Mlist])
+with open(outDir + "/NS.ids", 'w') as file:
+	file.writelines(["%s\t" % item  for item in NSlist])
+
+ARR = ["H1", "H3", "N1", "N2", "PB2", "PB1", "PA", "NP", "M", "NS"]
+   
